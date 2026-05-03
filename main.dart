d16f2c31ec1c3a35ac86ba6684ea86aad83480ae
Obbo Joseph <obboading66@gmail.com>
2026-05-03 14:53:39 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,255 +1,70 @@
 import 'package:flutter/material.dart';
-import 'chart.dart';
 
 void main() {
-  runApp(const Home());
+  runApp(const MediQuickApp());
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class MediQuickApp extends StatelessWidget {
+  const MediQuickApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'MediQuick',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const HomePage(),
       debugShowCheckedModeBanner: false,
-      home: ChatListScreen(),
     );
   }
 }
 
-class ChatListScreen extends StatefulWidget {
-  const ChatListScreen({super.key});
-
-  @override
-  State<ChatListScreen> createState() => _ChatListScreenState();
-}
-
-class _ChatListScreenState extends State<ChatListScreen> {
-  int _selectedIndex = 0;
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final contacts = [
-      {'name': 'Byamukama Bens', 'time': 'Last week', 'messages': '3'},
-      {'name': 'Justine Namome', 'time': 'Yesterday', 'messages': '1'},
-      {'name': 'Batasi Yosia', 'time': '09:52 AM', 'messages': '5'},
-      {'name': 'Babeiha Joseph', 'time': '10:15 AM', 'messages': '5'},
-      {'name': 'Nakabirwa Juliet', 'time': '10:15 AM', 'messages': '5'},
-      {'name': 'Makembo Jonah', 'time': '11:05 PM', 'messages': '5'},
-    ];
-
-    int totalUnread = 0;
-    for (var contact in contacts) {
-      totalUnread += int.parse(contact['messages']!);
-    }
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text("WhatsApp"),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.camera_alt_outlined),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.more_vert),
-          ),
-        ],
+        title: const Text('MediQuick'),
+        centerTitle: true,
       ),
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search...',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to MediQuickApp!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              '"Compassion" and "Empathy"',
+              style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+            ),
+            const SizedBox(height: 20),
+            const Icon(
+              Icons.medical_services,
+              size: 100,
+              color: Colors.teal,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Emergency button pressed!')),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
               ),
+              child: const Text('Emergency'),
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: contacts.length,
-              itemBuilder: (context, index) {
-                final contact = contacts[index];
-                return ListTile(
-                  leading: const CircleAvatar(
-                    child: Icon(Icons.person),
-                  ),
-                  title: Text(contact['name']!),
-                  subtitle: Text(contact['time']!),
-                  trailing: Badge(
-                    label: Text(contact['messages']!),
-                    backgroundColor: Colors.green,
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ChatScreen(
-                          contactName: contact['name']!,
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: totalUnread > 0
-                ? Badge(
-                    label: Text(totalUnread.toString()),
-                    backgroundColor: Colors.green,
-                    child: const Icon(Icons.chat),
-                  )
-                : const Icon(Icons.chat),
-            label: 'Chats',
-            backgroundColor: Colors.grey,
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.update),
-            label: 'Updates',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.people),
-            label: 'Communities',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.call),
-            label: 'Calls',
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const ChartPage()),
-          );
-        },
-        backgroundColor: Colors.green,
-        child: const Icon(Icons.add_box),
-      ),
-    );
-  }
-}
-
-class ChatScreen extends StatefulWidget {
-  final String contactName;
-
-  const ChatScreen({super.key, required this.contactName});
-
-  @override
-  State<ChatScreen> createState() => _ChatScreenState();
-}
-
-class _ChatScreenState extends State<ChatScreen> {
-  final TextEditingController _messageController = TextEditingController();
-  final List<Map<String, String>> messages = [
-    {'sender': 'other', 'text': 'Hey, how are you Obbo..?'},
-    {'sender': 'other', 'text': 'Did you get my message?'},
-    {'sender': 'me', 'text': 'Hi! I\'m good, thanks for asking'},
-    {'sender': 'me', 'text': 'however, I did not get it!'},
-    {'sender': 'other', 'text': 'oooh..! Let\'s meet up soon then'},
-  ];
-
-  void _sendMessage() {
-    if (_messageController.text.isNotEmpty) {
-      setState(() {
-        messages.add({'sender': 'me', 'text': _messageController.text});
-      });
-      _messageController.clear();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.contactName),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.call),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.video_call),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              reverse: true,
-              itemCount: messages.length,
-              itemBuilder: (context, index) {
-                final message = messages[messages.length - 1 - index];
-                final isMe = message['sender'] == 'me';
-                return Align(
-                  alignment:
-                      isMe ? Alignment.centerRight : Alignment.centerLeft,
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 12.0, vertical: 8.0),
-                    padding: const EdgeInsets.all(12.0),
-                    decoration: BoxDecoration(
-                      color: isMe ? Colors.green[300] : Colors.grey[300],
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    child: Text(
-                      message['text']!,
-                      style: TextStyle(
-                        color: isMe ? Colors.white : Colors.black,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _messageController,
-                    decoration: InputDecoration(
-                      hintText: 'Type a message...',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 16.0, vertical: 12.0),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 12.0),
-                FloatingActionButton(
-                  onPressed: _sendMessage,
-                  backgroundColor: Colors.green,
-                  child: const Icon(Icons.send),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
